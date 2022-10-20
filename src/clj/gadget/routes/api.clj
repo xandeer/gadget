cedd748113a860d3cb0ebe881376e5aafeebbaa6
Kevin Du <kkxandeer@gmail.com>
2022-10-20 21:44:44 +0800

    Simplify the code

--- src/clj/gadget/routes/api.clj
+++ src/clj/gadget/routes/api.clj
@@ -1,11 +1,10 @@
 (ns gadget.routes.api
   (:require
-   [gadget.middleware :as middleware]
-   [ring.util.http-response :as response]))
+   [gadget.middleware :refer [wrap-formats]]))
 
 (def api-routes
   ["/api"
-   {:middleware [middleware/wrap-formats]}
+   {:middleware [wrap-formats]}
    ["/hello" {:get (fn [_]
                      {:status 200
                       :body {:message "hello"}})}]])
