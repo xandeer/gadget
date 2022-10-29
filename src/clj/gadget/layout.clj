458d765a6a10b635b20a1d9cb6a1ffb0a7baa297
Kevin Du <kkxandeer@gmail.com>
2022-10-29 16:44:29 +0800

    fix: remove useless markdown

--- src/clj/gadget/layout.clj
+++ src/clj/gadget/layout.clj
@@ -3,7 +3,6 @@
     [clojure.java.io]
     [selmer.parser :as parser]
     [selmer.filters :as filters]
-    [markdown.core :refer [md-to-html-string]]
     [ring.util.http-response :refer [content-type ok]]
     [ring.util.anti-forgery :refer [anti-forgery-field]]
     [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
@@ -11,8 +10,6 @@
 
 (parser/set-resource-path!  (clojure.java.io/resource "html"))
 (parser/add-tag! :csrf-field (fn [_ _] (anti-forgery-field)))
-(filters/add-filter! :markdown (fn [content] [:safe (md-to-html-string content)]))
-
 (defn render
   "renders the HTML template located relative to resources/html"
   [request template & [params]]
